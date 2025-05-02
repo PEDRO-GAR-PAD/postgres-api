a11d8709bff5fbbc2546f62623b357501b28bcc9
PEDRO-GAR-PAD <zs23004733@estudiantes.uv.mx>
2025-05-01 22:33:57 -0600

    api postgres release3

--- database/config/config.js
+++ database/config/config.js
@@ -13,13 +13,16 @@ module.exports = {
   development: {
     url: process.env.DEV_DATABASE_URL,
     dialect: 'postgres',
+    ...sslConfig
   },
   test: {
     url: process.env.TEST_DATABASE_URL,
     dialect: 'postgres',
+    ...sslConfig
   },
   production: {
     url: process.env.DATABASE_URL,
     dialect: 'postgres',
+    ...sslConfig
   },
-}
+};
